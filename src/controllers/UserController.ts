a7618b8a008dccfc4e2c73977f88850dcc3ed8ff
Mark <muchirimark2@gmail.com>
2023-07-20 23:52:53 +0300

    - Made the data schema

---
+++ src/controllers/UserController.ts
@@ -0,0 +1,25 @@
+import { UserModel } from "bd/users";
+
+export const getUsers = () => UserModel.find();
+
+export const getUserByEmail = (email: String) =>
+	UserModel.findOne({
+		email: email,
+	});
+
+export const getUserBySessionToken = (sessionToken: String) =>
+	UserModel.findOne({
+		"authentication.sessionToken": sessionToken,
+	});
+
+export const getUserById = (id: String) => UserModel.findById(id);
+
+export const creatUser = (values: Record<string, any>) =>
+	new UserModel(values).save().then((user) => user.toObject());
+
+export const deleteUserById = (id: String) =>
+	UserModel.findOneAndDelete({ _id: id });
+
+export const updateUserById = (id: String, values: Record<string, any>) =>
+	UserModel.findByIdAndUpdate(id, values);
+
