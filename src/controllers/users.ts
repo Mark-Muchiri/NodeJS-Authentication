415ba52f221ace2ed8405a61235807f38f93d909
Mark <muchirimark2@gmail.com>
2023-07-27 01:17:07 +0300

    made the user delete,patch feature

--- src/controllers/users.ts
+++ src/controllers/users.ts
@@ -1,6 +1,6 @@
 import express from "express";
 
-import { getUsers, deleteUserById } from "./UserController";
+import { getUsers, deleteUserById, getUserById } from "./UserController";
 
 export const getAllUsers = async (req: express.Request, res: express.Response) => {
 	try {
@@ -18,8 +18,8 @@ export const deleteUser = async (req: express.Request, res: express.Response) =>
 		const { id } = req.params;
 
 		if (typeof id !== 'string') {
-            return res.status(400).json({ error: 'Invalid ID parameter' });
-        }
+			return res.status(400).json({ error: 'Invalid ID parameter' });
+		}
 
 		const deletedUser = await deleteUserById(id);
 
@@ -27,5 +27,37 @@ export const deleteUser = async (req: express.Request, res: express.Response) =>
 	} catch (error) {
 		console.error(error);
 		return res.sendStatus(400);
+	}
+};
+
+export const updateUser = async (req: express.Request, res: express.Response) => {
+	try {
+		const { id } = req.params;
+		const { username } = req.body;
+
+		// to ensure `id` is provided and not unidentified
+		if (!id) {
+			return res.sendStatus(400);
+		}
+
+		if (!username) {
+			return res.sendStatus(400);
+		}
+
+		const user = await getUserById(id);
+
+		if (!user) {
+			return res.sendStatus(404); // Or return an appropriate response when the user is not found
+		}
+
+		user.username = username;
+
+		await user?.save();
+
+		return res.status(200).json(user).end();
+	} catch (error) {
+		console.error(error);
+		return res.sendStatus(400);
+
 	}
 };
--- src/middlewares/index.ts
+++ src/middlewares/index.ts
@@ -2,6 +2,29 @@ import express from "express";
 import { get, merge } from "lodash";
 import { getUserBySessionToken } from "../controllers/UserController";
 
+// Can only delete yourself. no other account can delete another account, only the logged in account
+export const isOwner = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
+	try {
+		const { id } = req.params;
+		const currentUserId = get(req, 'identity._id') as string | undefined;
+
+		if (!currentUserId) {
+			return res.sendStatus(403);
+		}
+
+		if (currentUserId.toString() !== id) {
+			return res.sendStatus(403);
+		}
+
+		return next();
+	} catch (error) {
+		console.error(error);
+		return res.sendStatus(400);
+
+	}
+};
+
+
 export const isAuthenticated = async (req: express.Request, res: express.Response, next: express.NextFunction) => {
 	try {
 		const sessionToken = req.cookies["MARK-AUTH"];
